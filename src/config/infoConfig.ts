5f6bfc348fe2644adb841ef7ba8105ab0c89eadd
knightzz <qiuruizhe@qiuruizhedeMacBook-Pro.local>
2025-04-13 18:17:45 -0400

    描述你这次更新的内容

--- src/config/infoConfig.ts
+++ src/config/infoConfig.ts
@@ -4,25 +4,25 @@ export * from './career'
 
 // personal info
 export const name = 'Ruizhe Qiu'
-export const headline = 'Student at Duke University.'
+export const headline = 'A Master Student Graduated From Duke University.'
 export const introduction =
-  "Coucou Les Amis! My name is Ruizhe Qiu. I'm from Jiangsu and an alumnus of NFLS, NYU, and Duke. My passion is in Risk Management, Compliance & Judo."
+  "Coucou Les Amis! My name is Ruizhe Qiu. I'm from China and an alumnus of UMN-Twin Cities, and Duke. My passion is in Risk Management, Finance and Data Science."
 export const email = 'ruizhe.qiu@duke.edu'
 export const githubUsername = 'HenryRorschach'
 
 // about page
 export const aboutMeHeadline = 'Who Are You and Why Should I Care?'
 export const aboutParagraphs = [
-  "Hey there! I’m Ruizhe Qiu — born in China, trained in tech in NYC, and now diving into the world of business analytics at Duke University. My journey bridges the gap between code and strategy, with a B.A. from NYU and current grad studies at Fuqua.",
-  "Right now, I'm building real-time risk tracking systems, automating finance with RPA, and pushing the frontier of AI chatbot adoption at PwC. I love turning complex problems into elegant solutions.",
-  "This site is my digital dojo — part journal, part lab. Expect thoughts on AI, strategy, data, and sometimes, judo. Welcome to the brain of someone who believes tech should be both powerful and human.",
+"Hey there! I’m Ruizhe Qiu — born in China, trained in finance and analytics in Minnesota, and  sharpened my skills in business analytics at Duke University’s Fuqua School of Business. My journey blends quantitative insight with social impact, from internships to community redevelopment in the U.S. and China.",
+"Right now, I used to build financial models for multi-million-dollar urban projects, decoding industry trends at SPD Bank, and delivering strategic analysis for public-private partnerships. I thrive at the intersection of data, risk, and innovation.",
+"This site is where data meets purpose — a space to explore risk, finance, and the narratives behind the numbers. Whether building strategic models or supporting community growth, I believe good analysis drives real-world impact.",
 ];
 
 
 // blog
 export const blogHeadLine = "What I've thinking about."
 export const blogIntro =
-  "I've written something about AI, programming and life."
+  "I've written something about AI, programming, finance and my personal life."
 
 // social links
 export type SocialLinkType = {
@@ -32,18 +32,6 @@ export type SocialLinkType = {
   href: string
 }
 
-export const socialLinks: Array<SocialLinkType> = [
-  {
-    name: 'Tiktok',
-    icon: 'tiktok',
-    href: 'https://www.tiktok.com/@harvard?lang=en',
-  },
-  {
-    name: 'Bilibili',
-    icon: 'bilibili',
-    href: 'https://space.bilibili.com/23740961',
-  },
-]
 
 // https://simpleicons.org/
 export const techIcons = [
